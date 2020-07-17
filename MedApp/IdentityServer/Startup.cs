9633d69a1efa48d300a7b17b9df475b635a763cf
Kristijan Delač <34710959+kdelac@users.noreply.github.com>
2020-07-17 15:39:19 +0200

    promjena

--- MedApp/IdentityServer/Startup.cs
+++ MedApp/IdentityServer/Startup.cs
@@ -2,11 +2,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MedAppCore.Models;
+using MedAppData;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNet.Identity;
 
 namespace IdentityServer
 {
@@ -16,12 +20,13 @@ namespace IdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<MedAppDbContext>()
+                .AddDefaultTokenProviders();
+
             services.AddIdentityServer()
-                .AddInMemoryApiScopes(Configuration.GetApiScopes())
-                .AddInMemoryApiResources(Configuration.GetApiResources())
-                .AddInMemoryIdentityResources(Configuration.GetIdentityResources())
-                .AddTestUsers(Configuration.GetUsers())
-                .AddInMemoryClients(Configuration.GetClients())
+                .AddAspNetIdentity<ApplicationUser>()
+                
                 .AddDeveloperSigningCredential();
 
             services.AddControllers();
