0334693a8fc6f0b6d33a661e23ff1379d4f40053
Kristijan Delač <34710959+kdelac@users.noreply.github.com>
2020-08-31 12:40:50 +0200

    uklonjene neke klase

--- MedApp/WebApi/Controllers/WeatherForecastController.cs
+++ MedApp/WebApi/Controllers/WeatherForecastController.cs
@@ -16,30 +16,9 @@ namespace WebApi.Controllers
     {
         private readonly IApiCall _apiCall;
 
-        private static readonly string[] Summaries = new[]
+        public WeatherForecastController(IApiCall apiCall)
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        private readonly ILogger<WeatherForecastController> _logger;
-
-        public WeatherForecastController(ILogger<WeatherForecastController> logger, IApiCall apiCall)
-        {
-            _logger = logger;
             _apiCall = apiCall;
-        }
-
-        [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
         }
 
         [HttpPost]
