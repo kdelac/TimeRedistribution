afe05024e3776aa636f9d72d4a9dcc2c68656ae2
Kristijan Delač <34710959+kdelac@users.noreply.github.com>
2020-10-20 15:50:47 +0200

    potpis skoro radi

---
+++ MedApp/Signing/ExternalHashingSignatureContainer.cs
@@ -0,0 +1,26 @@
+﻿using iText.Kernel.Pdf;
+using iText.Signatures;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Signing
+{
+    public class ExternalHashingSignatureContainer : ExternalBlankSignatureContainer
+    {
+        public ExternalHashingSignatureContainer(PdfName filter, PdfName subFilter) : base(filter, subFilter)
+        { }
+
+        public override byte[] Sign(Stream data)
+        {
+            SHA256 sha = new SHA256CryptoServiceProvider();
+            String hashAlgorithm = DigestAlgorithms.SHA256;
+            Hash = DigestAlgorithms.Digest(data, DigestAlgorithms.GetMessageDigest(hashAlgorithm));
+            return new byte[0];
+        }
+
+        public byte[] Hash { get; private set; }
+    }
+}
---
+++ MedApp/Signing/MyExternalSignatureContainer.cs
@@ -0,0 +1,46 @@
+﻿using iText.Kernel.Pdf;
+using iText.Signatures;
+using Org.BouncyCastle.X509;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Signing
+{
+    class MyExternalSignatureContainer : IExternalSignatureContainer
+    {
+        protected X509Certificate[] chain;
+        protected byte[] signedHash;
+        protected byte[] hash;
+
+        public MyExternalSignatureContainer(byte[] signedHash, X509Certificate[] chain, byte[] hash)
+        {
+            this.signedHash = signedHash;
+            this.chain = chain;
+            this.hash = hash;
+        }
+
+        public byte[] Sign(Stream inputStream)
+        {
+            try
+            {
+                String hashAlgorithm = DigestAlgorithms.SHA256;
+                PdfPKCS7 sgn = new PdfPKCS7(null, chain, hashAlgorithm, false);
+                byte[] hashh = DigestAlgorithms.Digest(inputStream, hashAlgorithm);
+                sgn.SetExternalDigest(signedHash, null, "RSA");
+
+                return sgn.GetEncodedPKCS7(hashh, PdfSigner.CryptoStandard.CMS, null,
+                    null, null);
+            }
+            catch (IOException ioe)
+            {
+                throw new Exception(ioe.Message);
+            }
+        }
+
+        public void ModifySigningDictionary(PdfDictionary signDic)
+        {
+        }
+    }
+}
